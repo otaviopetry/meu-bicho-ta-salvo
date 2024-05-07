41d8099fbbc21b4064a892bb344e5fc7adda1cda
otaviopetry <otaviopetry@gmail.com>
2024-05-07 19:36:56 -0300

    fix: remove unused import

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IAnimal } from '../interfaces/animal.interface';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
