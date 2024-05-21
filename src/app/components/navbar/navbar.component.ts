58eb09725aa7ac7ea5d2674e68f658bb35870dbb
otaviopetry <otaviopetry@gmail.com>
2024-05-21 02:10:09 -0300

    feat: close toggle menu on outside click

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarDesktopComponent } from './navbar-desktop/navbar-desktop.component';
 import { NavbarMobileComponent } from './navbar-mobile/navbar-mobile.component';
@@ -19,7 +19,21 @@ import { NavbarMobileComponent } from './navbar-mobile/navbar-mobile.component';
 export class NavbarComponent {
   menuOpen = false;
 
+  constructor(private elementRef: ElementRef) {
+    //
+  }
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    if (
+      this.menuOpen &&
+      !this.elementRef.nativeElement.contains(event.target)
+    ) {
+      this.menuOpen = false;
+    }
   }
 }
